8ec00e3cd0a24ebd6ffafdb97edcca2b643f893a
sniperHW <sniperHW@163.com>
2023-08-18 23:33:39 +0800

    none

--- websocket.go
+++ websocket.go
@@ -1,6 +1,7 @@
 package netgo
 
 import (
+	"io"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -9,47 +10,42 @@ import (
 	gorilla "github.com/gorilla/websocket"
 )
 
-type webSocketReadable struct {
-	conn *gorilla.Conn
-	msg  []byte
-}
-
-func (readable *webSocketReadable) Read(buff []byte) (int, error) {
-	var err error
-	remain := len(readable.msg)
-	if remain == 0 {
-		_, readable.msg, err = readable.conn.ReadMessage()
-		if nil != err {
-			return 0, err
-		} else {
-			remain = len(readable.msg)
-		}
-	}
-
-	if remain > len(buff) {
-		copy(buff, readable.msg[:len(buff)])
-		readable.msg = readable.msg[len(buff):]
-		return len(buff), nil
-	} else {
-		copy(buff, readable.msg)
-		readable.msg = nil
-		return remain, nil
-	}
-}
-
-func (readable *webSocketReadable) SetReadDeadline(deadline time.Time) error {
-	return readable.conn.SetReadDeadline(deadline)
-}
-
 type webSocket struct {
 	userData       atomic.Value
 	packetReceiver PacketReceiver
 	conn           *gorilla.Conn
+	reader         io.Reader
 	closeOnce      sync.Once
-	readableObj    *webSocketReadable
 }
 
 var _ Socket = &webSocket{}
+
+func (wc *webSocket) Read(buff []byte) (int, error) {
+	if wc.reader == nil {
+		_, r, err := wc.conn.NextReader()
+		if err != nil {
+			return 0, err
+		}
+		wc.reader = r
+	}
+
+	n, err := wc.reader.Read(buff)
+	if err != nil && err != io.EOF {
+		return n, err
+	} else if err == io.EOF {
+		_, r, err := wc.conn.NextReader()
+		if err != nil {
+			return 0, err
+		}
+		wc.reader = r
+	}
+
+	return n, nil
+}
+
+func (wc *webSocket) SetReadDeadline(deadline time.Time) error {
+	return wc.conn.SetReadDeadline(deadline)
+}
 
 func (wc *webSocket) LocalAddr() net.Addr {
 	return wc.conn.LocalAddr()
@@ -102,37 +98,23 @@ func (wc *webSocket) Send(data []byte, deadline ...time.Time) (int, error) {
 }
 
 func (wc *webSocket) Recv(deadline ...time.Time) (packet []byte, err error) {
-	if nil == wc.readableObj {
-		d := time.Time{}
-		if len(deadline) > 0 && !deadline[0].IsZero() {
-			d = deadline[0]
-		}
-		if err = wc.conn.SetReadDeadline(d); err != nil {
-			return
-		} else {
-			_, packet, err = wc.conn.ReadMessage()
-		}
+	if len(deadline) > 0 && !deadline[0].IsZero() {
+		packet, err = wc.packetReceiver.Recv(wc, deadline[0])
 	} else {
-		if len(deadline) > 0 && !deadline[0].IsZero() {
-			packet, err = wc.packetReceiver.Recv(wc.readableObj, deadline[0])
-		} else {
-			packet, err = wc.packetReceiver.Recv(wc.readableObj, time.Time{})
-		}
+		packet, err = wc.packetReceiver.Recv(wc, time.Time{})
 	}
 	return
 }
 
 func NewWebSocket(conn *gorilla.Conn, packetReceiver ...PacketReceiver) Socket {
-	s := &webSocket{
+	ws := &webSocket{
 		conn: conn,
 	}
+	if len(packetReceiver) == 0 || packetReceiver[0] == nil {
+		ws.packetReceiver = &defaultPacketReceiver{}
+	} else {
+		ws.packetReceiver = packetReceiver[0]
 
-	if len(packetReceiver) > 0 && packetReceiver[0] != nil {
-		s.packetReceiver = packetReceiver[0]
-		s.readableObj = &webSocketReadable{
-			conn: conn,
-		}
 	}
-
-	return s
+	return ws
 }
